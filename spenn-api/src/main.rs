129ed6f19cdcc2fd5822cc2affb0e23641b3b52f
Joseph Beck <joebeck03@icloud.com>
2024-04-03 22:43:43 +0100

    feat: init actix api

--- spenn-api/src/main.rs
+++ spenn-api/src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{App, HttpServer};
+
+mod services;
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().service(services::get_root))
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
 }
---
+++ spenn-api/src/services.rs
@@ -0,0 +1,25 @@
+use actix_web::{get, HttpResponse, Responder};
+
+#[get("/api/v1")]
+async fn get_root() -> impl Responder {
+    HttpResponse::Ok().body(format!("healthy"))
+}
+
+#[cfg(test)]
+mod tests {
+    use actix_web::{test, App};
+
+    use super::*;
+
+    #[actix_web::test]
+    async fn test_get_root() {
+        let app = test::init_service(App::new().service(get_root)).await;
+        let req = test::TestRequest::get().uri("/api/v1").to_request();
+        let resp = test::call_service(&app, req).await;
+        assert!(
+            resp.status().is_success(),
+            "status code {:?}",
+            resp.status()
+        );
+    }
+}
