25461e0a9dc590fba2e06118b7e1240dd39de7bd
Joseph Beck <joebeck03@icloud.com>
2024-04-03 22:51:06 +0100

    feat: init logger

--- spenn-api/src/main.rs
+++ spenn-api/src/main.rs
@@ -1,11 +1,22 @@
-use actix_web::{App, HttpServer};
+use actix_web::{middleware::Logger, App, HttpServer};
+use dotenv::dotenv;
+use env_logger::init;
+use std::env;
 
 mod services;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(services::get_root))
-        .bind(("127.0.0.1", 8080))?
-        .run()
-        .await
+    dotenv().ok();
+    env::set_var("RUST_LOG", "actix_web=debug,actix_server=info");
+    init();
+
+    HttpServer::new(|| {
+        App::new()
+            .wrap(Logger::default())
+            .service(services::get_root)
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
