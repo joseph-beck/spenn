268625425a12150624c96acd3cd4ab5e9cddf405
Joseph Beck <joebeck03@icloud.com>
2024-04-05 00:39:03 +0100

    feat: add migrate to mac

--- spenn-entity/src/mac.rs
+++ spenn-entity/src/mac.rs
@@ -1,22 +1,17 @@
-use sea_orm::entity::prelude::*;
+use sea_orm::{entity::prelude::*, Schema};
 use serde::{Deserialize, Serialize};
 
 #[derive(Clone, Debug, PartialEq, Eq, DeriveEntityModel, Deserialize, Serialize)]
 #[sea_orm(table_name = "macs")]
 pub struct Model {
-    #[sea_orm(primary_key)]
-    #[sea_orm(column_type = "Uuid")]
+    #[sea_orm(primary_key, auto_increment = false)]
+    #[sea_orm(column_type = "Text")]
     pub uuid: Uuid,
     #[sea_orm(column_type = "Text")]
     pub name: String,
     #[sea_orm(column_type = "Text")]
     pub address: String,
 }
-
-#[derive(Copy, Clone, Debug, EnumIter, DeriveRelation)]
-pub enum Relation {}
-
-impl ActiveModelBehavior for ActiveModel {}
 
 impl Model {
     pub fn new(name: String, address: String) -> Model {
@@ -26,7 +21,31 @@ impl Model {
             address,
         }
     }
+
+    pub fn default() -> Model {
+        Model {
+            uuid: Uuid::new_v4(),
+            name: "name".to_string(),
+            address: "address".to_string(),
+        }
+    }
+
+    pub async fn migrate(db: &DbConn) -> Result<(), DbErr> {
+        let backend = db.get_database_backend();
+        let schema = Schema::new(backend);
+        let mut create = schema.create_table_from_entity(self::Entity);
+
+        match db.execute(backend.build(create.if_not_exists())).await {
+            Ok(_) => Ok(()),
+            Err(err) => Err(err),
+        }
+    }
 }
+
+#[derive(Copy, Clone, Debug, EnumIter, DeriveRelation)]
+pub enum Relation {}
+
+impl ActiveModelBehavior for ActiveModel {}
 
 #[derive(Debug, Clone, PartialEq, Eq, Deserialize, Serialize)]
 pub struct Request {
